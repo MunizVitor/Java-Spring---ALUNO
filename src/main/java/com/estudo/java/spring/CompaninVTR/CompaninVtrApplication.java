1cfd042ad4b102d068093b16d59fa8907c569756
MunizVitor <vmuniz.dev@gmail.com>
2026-01-24 20:10:09 -0300

    subindo o codigo para começar a codar

---
+++ src/main/java/com/estudo/java/spring/CompaninVTR/CompaninVtrApplication.java
@@ -0,0 +1,13 @@
+package com.estudo.java.spring.CompaninVTR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompaninVtrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompaninVtrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudo/java/spring/CompaninVTR/CompaninVtrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudo.java.spring.CompaninVTR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompaninVtrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
